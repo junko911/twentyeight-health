83fbd8c2121169692977b46a8330311da6c76285
junko911 <junkotahara911@gmail.com>
2021-04-25 21:09:01 -0400

    Catch the errors

--- app/services/doctor_assigner.rb
+++ app/services/doctor_assigner.rb
@@ -1,20 +1,27 @@
 class DoctorAssigner
   def self.call(patient)
-    url = URI("https://challenge-28h.herokuapp.com/doctors/assign")
-    https = Net::HTTP.new(url.host, url.port)
-    https.use_ssl = true
+    begin
+      url = URI("https://challenge-28h.herokuapp.com/doctors/assign")
+      https = Net::HTTP.new(url.host, url.port)
+      https.use_ssl = true
 
-    request = Net::HTTP::Post.new(url)
-    request["Content-Type"] = "application/json"
-    request.body = {
-      name: patient.name,
-      email: patient.email,
-      date_of_birth: patient.date_of_birth,
-      state: patient.state.abbreviation
-    }.to_json
+      request = Net::HTTP::Post.new(url)
+      request["Content-Type"] = "application/json"
+      request.body = {
+        name: patient.name,
+        email: patient.email,
+        date_of_birth: patient.date_of_birth,
+        state: patient.state.abbreviation
+      }.to_json
 
-    response = https.request(request)
-    details = JSON.parse(response.read_body)["details"]
-    patient.update(doctor: details["doctor"], token: details["token"])
+      response = https.request(request)
+      details = JSON.parse(response.read_body)["details"]
+      patient.update(doctor: details["doctor"], token: details["token"])
+      
+    rescue StandardError => e
+      puts e
+
+      false
+    end
   end
 end
